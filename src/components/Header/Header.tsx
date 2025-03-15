9e8b872643e167ca4fd2d2ac0198d77444f3bbad
Bruno Gomes <obrunogomes@obrunogomes.local>
2025-03-15 12:18:21 -0300

    feat: criando header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,9 @@
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-7xl font-bold text-green-800">Header</h1>
-    </div>
+   <header className="w-full h-28 bg-quaternary border-b border-primary/10">
+      <h1>Header</h1>
+   </header>
   )
 }
-
 export default Header
