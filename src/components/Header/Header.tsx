d98354d9dbf0675d870506086053eb391f042d96
Bruno Gomes <bgomes.89@gmail.com>
2025-03-15 16:02:57 -0300

    feat: add hover no menu e link para navegação entre as páginas

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
 import logo from "../../assets/logo.svg"
 import lines from "../../assets/lineDetails.svg"
 import { Nav } from "../Nav/nav"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
    <header className="w-full h-28 bg-quaternary border-b border-primary/10">
       <div className="flex justify-between items-center px-6">
         <img src={lines} alt="" />
-        <img src={logo} alt="logo" />
+
+        <Link to='/'>
+            <img src={logo} alt="logo" />
+        </Link>
         <Nav />
       </div>
    </header>
--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 export function Nav(){
     return (
         <ul className="flex gap-3 uppercase">
-          <li className="text-md text-primary font-bold hover:cursor-pointer">Sobre</li>
-          <li className="text-md text-primary font-bold hover:cursor-pointer">Contato</li>
+          <Link to="/sobre">
+             <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-active">Sobre</li>
+          </Link>
+          <Link to="/contato">
+             <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-active">Contato</li>
+          </Link>
         </ul>
     )
 }
