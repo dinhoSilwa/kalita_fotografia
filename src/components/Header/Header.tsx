1b13a63aaf5e3b69a10278f205ab859cc1baa93b
Bruno Gomes <bgomes.89@gmail.com>
2025-03-15 13:45:34 -0300

    feat: header iniciado, logo e menu estaticos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
-
+import logo from "../../assets/logo.svg"
+import lines from "../../assets/lineDetails.svg"
+import { Nav } from "../Nav/nav"
 
 const Header = () => {
   return (
    <header className="w-full h-28 bg-quaternary border-b border-primary/10">
-      <h1>Header</h1>
+      <div className="flex justify-between items-center px-6">
+        <img src={lines} alt="" />
+        <img src={logo} alt="logo" />
+        <Nav />
+      </div>
    </header>
   )
 }
---
+++ src/components/Nav/nav.tsx
@@ -0,0 +1,8 @@
+export function Nav(){
+    return (
+        <ul className="flex gap-3 uppercase">
+          <li className="text-md text-primary font-bold hover:cursor-pointer">Sobre</li>
+          <li className="text-md text-primary font-bold hover:cursor-pointer">Contato</li>
+        </ul>
+    )
+}
