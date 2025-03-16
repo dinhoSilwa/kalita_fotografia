1fc946ee8767a01edf0b3857a705d343ccf6a318
Bruno Gomes <bgomes.89@gmail.com>
2025-03-16 11:19:23 -0300

    style: ajuste max-width

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
    <header className="w-full h-28 bg-quaternary border-b border-primary/10">
-      <div className="flex justify-between items-center px-6">
+      <div className="flex justify-between items-center px-6 max-w-[1440px] mx-auto">
         <img src={lines} alt="" />
 
         <Link to='/'>
