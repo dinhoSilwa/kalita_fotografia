3b7275b6c77047291b0891540920e76deff9b537
Lucas Gomes <lucaass1997rp@gmail.com>
2025-03-16 11:32:05 -0300

    feat: cria estrutura inicial da tela de contato

--- src/pages/Contato/Contato.tsx
+++ src/pages/Contato/Contato.tsx
@@ -1,16 +1,27 @@
 import Card from "./components/Card"
 
+
 const Contato = () => {
   return (
-    <section>
-      <div>
-        <h1>Quer agendar uma sessão?</h1>
-        <p>Entre em contato conosco</p>
+    <section className="max-w-7xl h-[calc(100vh-7rem)] bg-quaternary flex justify-center items-center flex-col m-auto">
+      <div className="w-7xl h-[50%] flex justify-center items-center flex-col gap-6 bg-[#D9D9D9]">
+        <h1 className="w-[556px] text-6xl font-bold text-[#545454]">Quer agendar uma sessão?</h1>
+        
+        <div className="w-[556px]">
+          
+          <div className="text-3xl font-bold text-[#533E29] relative after:content-[''] after:w-[98px] after:h-[1px] after:bg-black after:absolute after:left-[0px] after:top-[20px] after:translate-y-[-50%]">
+
+          </div>
+
+          <p className="text-3xl font-bold text-[#242424] relative ml-[105px]">Entre em contato conosco</p>
+        </div>
+        
       </div>
-      <div>
+      <div className="bg-black  w-full h-[50%]">
+        {/* <Card />
         <Card />
-        <Card />
-        <Card />
+        <Card /> */}
+        <h1 className="text-amber-50">Cards</h1>
       </div>
     </section>
   )
