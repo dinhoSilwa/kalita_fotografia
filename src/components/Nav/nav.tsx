f0a765259f1b370b39a7e49743857bb3ea948a9d
Bruno Gomes <bgomes.89@gmail.com>
2025-07-01 18:52:20 -0300

    feat: menu adaptando para mobile iniciado

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -2,15 +2,37 @@ import { Link } from "react-router-dom";
 
 export function Nav(){
     return (
-      <nav className="absolute right-6">
-          <ul className="flex gap-3 uppercase">
+      <nav className="absolute right-6 ">
+          <ul className="gap-3 uppercase hidden sm:flex">
+            <Link to="/portfolio">
+              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Portfólio</li>
+            </Link>
             <Link to="/sobre">
               <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Sobre</li>
+            </Link>
+            <Link to="/depoimentos">
+              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Depoimentos</li>
             </Link>
             <Link to="/contato">
               <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Contato</li>
             </Link>
           </ul>
+
+          <ul className="gap-3 uppercase sm:hidden flex flex-col items-end mt-70">
+            <Link to="/portfolio">
+              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Portfólio</li>
+            </Link>
+            <Link to="/sobre">
+              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Sobre</li>
+            </Link>
+            <Link to="/depoimentos">
+              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Depoimentos</li>
+            </Link>
+            <Link to="/contato">
+              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Contato</li>
+            </Link>
+          </ul>
+          
         </nav>
     )
 }
