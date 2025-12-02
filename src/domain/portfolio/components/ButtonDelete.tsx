eb11aae8e70c8442e866ecb1984010bca34b6397
Daniel <danielpapereiraJV@gmail.com>
2025-12-02 10:37:42 -0300

    feat/upload-image: Updated delete button - UI.

--- src/domain/portfolio/components/ButtonDelete.tsx
+++ src/domain/portfolio/components/ButtonDelete.tsx
@@ -16,7 +16,6 @@ export default function DeleteButtonPortfolio() {
       removeImages([selectedIds[0]]);
       return;
     }
-
     setOpen(true);
   }
 
@@ -26,27 +25,30 @@ export default function DeleteButtonPortfolio() {
         onClick={handleClick}
         className="
           flex items-center gap-2
-          px-4 py-2
-          bg-red-600
-          text-white 
+          px-4 md:px-5 py-2 md:py-2.5
           rounded-md
+          bg-red-600 text-kalita-bg-light
+          font-nunito 
+          cursor-pointer
+          transition-all duration-200
           hover:bg-red-700
-          transition
         "
       >
-        <FiTrash2 size={18} />
-        <span>Excluir</span>
+        <FiTrash2 size={18} className="shrink-0" />
 
-        <span className="text-sm opacity-90">
+        <span className="text-sm md:text-base whitespace-nowrap">
+          Excluir
+        </span>
+
+        <span className="text-xs md:text-sm opacity-90">
           ({selectedIds.length})
         </span>
       </button>
 
-      {/* MODAL */}
       <DeleteModal
         open={open}
         onClose={() => setOpen(false)}
-        ids={selectedIds} 
+        ids={selectedIds}
       />
     </>
   );
