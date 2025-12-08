8c733cbd558959a78844e0f8380c787bd65faca0
dinhoSiwa <claudiosilva.one@gmail.com>
2025-12-08 16:30:49 -0300

    refactor: add-correct-type

--- src/domain/portfolio/hooks/useGetAllPhothos.ts
+++ src/domain/portfolio/hooks/useGetAllPhothos.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query"
 import { portfolioMutation } from "./useUploadMultiples"
+import type { PhotoInDb } from "../entities/portfolio-entities"
 
 export const useGetAllPhotos = () =>{
-  const {data, isError, isLoading, refetch} = useQuery({
+  const {data, isError, isLoading, refetch} = useQuery<PhotoInDb[]>({
     queryKey : ['all-photos'],
     queryFn : () => portfolioMutation.getAllPortfolioPhotos()
   })
-
   return {data, isError, isLoading, refetch}
 }
